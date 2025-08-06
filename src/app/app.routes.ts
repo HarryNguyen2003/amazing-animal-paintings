54587bb16e0461868a23cf8f9cd5a76349fab3a3
HieuFPT2003 <hieunqhe170048@fpt.edu.vn>
2025-08-07 02:08:21 +0700

    feat(question) UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductList } from './page/product/product-list/product-list';
 import { CartComponent } from './page/cart/cart';
+import { QuestionComponent } from './page/question/question';
 
 export const routes: Routes = [
 
@@ -8,5 +9,6 @@ export const routes: Routes = [
     // khi là path rỗng nó tự redirectTo products
     {path: '',  redirectTo: '/products', pathMatch: 'full'},
     {path: 'products', component:ProductList},
-    {path:'cart',component:CartComponent}
+    {path:'cart',component:CartComponent},
+    {path:'question',component:QuestionComponent}
 ];
---
+++ src/app/page/question/question.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, FormArray, Validators } from '@angular/forms'
+import { TextareaModule } from 'primeng/textarea';
+
+@Component({
+  selector: 'app-question',
+  imports: [ReactiveFormsModule,TextareaModule,CommonModule],
+  templateUrl: './question.html',
+  styleUrl: './question.css'
+})
+export class QuestionComponent {
+  formCreateFormQuestion: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formCreateFormQuestion = this.fb.group({
+      // éo nên ép khải báo questions là 
+      questions: this.fb.array([])
+    })
+
+    this.createQuestionItem()
+  }
+
+  // t tạo 1 getter để lấy ra thằng formArray questions từ formCreateFormQuestion
+  get questions(): FormArray {
+
+    // éo thể truy cập từ this vì thằng questions éo phải là một biến class cx éo phải thược tính mặc định nó chỉ là 1 key trong form
+    // Thêm "as FormArray" dùng để ép kiểu cho thằng này thành FormArray để dùng push(),....
+    return this.formCreateFormQuestion.get('questions') as FormArray;
+  }
+
+
+  createQuestionItem() {
+    const question = this.fb.group({
+      question: ['', Validators.required],
+      answerA: ['', Validators.required],
+      answerB: ['', Validators.required],
+      answerC: ['', Validators.required],
+      answerD: ['', Validators.required]
+    });
+
+    this.questions.push(question);
+  }
+
+
+  // remove theo index nhe
+  removeQuestion(index: number) {
+    this.questions.removeAt(index)
+  }
+
+  onSubmit() {
+    if (this.formCreateFormQuestion.valid) {
+      console.log('Form Question', this.formCreateFormQuestion.value)
+    }
+  }
+
+}
